46981ff9afe1f003f45cc63be02dad2ca1ca00f9
Nicholas <43795440+Loxodograph@users.noreply.github.com>
2025-01-03 18:53:37 +0000

    frequencyPEDAC

---
+++ SPOT_EXERCISES/frequencyDistribution.js
@@ -0,0 +1,41 @@
+/*
+
+Problem
+  - Create a function that returns the frequency 
+    distribution of an array.
+
+  - Input: Array
+  - Output: Object - keys are unique elements, values are
+            frequency in which elements occur
+
+  - Rules
+    - Explicit:
+      - If given an empty array, return an empty object
+      - The object should be in the same order as 
+        in the input array.
+
+    - Implicit:
+      
+
+  - Questions:
+    
+
+Examples and Test Cases
+
+getFrequencies(["A", "B", "A", "A", "A"]) // { A: 4, B: 1 }
+
+getFrequencies([1, 2, 3, 3, 2]) // { "1": 1, "2": 2, "3": 2 }
+
+getFrequencies([true, false, true, false, false]) // { true: 2, false: 3 }
+
+getFrequencies([]) // {}
+
+
+Data Structures
+
+arrays and objects
+
+Algorithm
+
+Code
+*/
