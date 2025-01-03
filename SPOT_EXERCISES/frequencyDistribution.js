bf981db711dd92745e1678e701a6c8fb7ae337a5
Nicholas <43795440+Loxodograph@users.noreply.github.com>
2025-01-03 18:59:33 +0000

    frequencyDistribution.js

--- SPOT_EXERCISES/frequencyDistribution.js
+++ SPOT_EXERCISES/frequencyDistribution.js
@@ -36,6 +36,36 @@ Data Structures
 arrays and objects
 
 Algorithm
+  - create function getFrequencies which takes array as argument
+    - create object initialized to empty object
+    - iterate over elements of inputArray
+      -if empty object contains element
+        - increment value of object at element
+      - else
+        - create value 1 of object at element
+    - return object
 
 Code
 */
+
+function getFrequencies(array) {
+  let frequencyObject = {};
+
+  for (let i = 0; i < array.length; i++) {
+    if (frequencyObject[array[i]]) {
+      frequencyObject[array[i]]++;
+    } else {
+      frequencyObject[array[i]] = 1;
+    }
+  }
+
+  return frequencyObject;
+}
+
+console.log(getFrequencies(["A", "B", "A", "A", "A"])) // { A: 4, B: 1 }
+
+console.log(getFrequencies([1, 2, 3, 3, 2])) // { "1": 1, "2": 2, "3": 2 }
+
+console.log(getFrequencies([true, false, true, false, false])) // { true: 2, false: 3 }
+
+console.log(getFrequencies([])) // {}
