0c647ae4f05405ea32265b1f4331ee80c32c31f5
Nicholas <43795440+Loxodograph@users.noreply.github.com>
2025-01-03 18:34:16 +0000

    missingfromArray

---
+++ SPOT_EXERCISES/missingFromArray.js
@@ -0,0 +1,37 @@
+/*
+
+Problem
+  - You are given two arrays. The elements in arr1 threw a party and 
+    started to mix around. However, one of the elements got lost! 
+    Your task is to return the element which was lost.
+
+  - Input: 
+  - Output: 
+
+  - Rules
+    - Explicit:
+
+    - Implicit:
+      
+
+  - Questions
+    
+
+Examples and Test Cases
+
+missing([1, 2, 3, 4, 5, 6, 7, 8], [1, 3, 4, 5, 6, 7, 8]) // 2
+
+missing([true, true, false, false, true], [false, true, false, true]) // true
+
+missing(["Jane", "is", "pretty", "ugly"], ["Jane", "is", "pretty"]) // "ugly"
+
+
+
+Data Structures
+
+
+
+Algorithm
+
+Code
+*/
