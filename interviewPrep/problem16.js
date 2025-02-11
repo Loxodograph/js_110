9f73ea3046c38499e477b58faf9f30f2ea1c7031
Nicholas <43795440+Loxodograph@users.noreply.github.com>
2025-02-11 12:55:27 +0000

    problem16

--- interviewPrep/problem16.js
+++ interviewPrep/problem16.js
@@ -3,8 +3,31 @@ P
   - create function returns count of distinct case-insentive characters and
     digits that occur more than once in string
 
+Input : String
+Output : Number
 
+Data structure, array and strings and numbers
+
+algorithm
+create function distinctMultiples which takes string argument
+define variable duplicateArray
+iterate through elements of string
+  if index of element is not equal to last index of element
+    push element to duplicateArray
+return duplicateArray.length
 */
+
+function distinctMultiples(string) {
+  let str = string.toLowerCase();
+  let duplicateArray = [];
+  for (let idx = 0; idx < str.length; idx++) {
+    if (str.indexOf(str[idx]) !== str.lastIndexOf(str[idx]) &&
+        !duplicateArray.includes(str[idx])) {
+      duplicateArray.push(str[idx]);
+    }
+  }
+  return duplicateArray.length;
+}
 
 const p = console.log;
 p(distinctMultiples('xyz') === 0);              // (none)
