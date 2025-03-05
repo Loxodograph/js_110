5d30fc815d5ebdd3dd5ac999dd10c211dd22ffcb
Loxodograph <paldino.nicholas@gmail.com>
2025-03-05 16:43:39 -0500

    x

--- Class/fourthClass/fourthClassRedo/sumOfNestedredo.js
+++ Class/fourthClass/fourthClassRedo/sumOfNestedredo.js
@@ -1,38 +1,48 @@
-// Write a function that calculates the sum of all numbers in a nested array.
-// The array may contain integers, nested arrays of integers, or a mix of
-// both. (for ruby and JS don’t use flatten)
-
 /*
+
 Problem
-  create function that returns sum of all numbers in nested array
-  Cannot use flatten
+  - Write a function that calculates the sum of all numbers in a nested array.
+    The array may contain integers, nested arrays of integers, or a mix of both.
+    (for ruby and JS don’t use flatten)
+
+  Input: A nested Array
+  Output: Sum of all integers in nested array
+
+  Rules
+    explicit:
+      - Can not use flatten
+      - all elements will be either integers or nested arrays of integers
+      - Return sum of numbers
+
+Examples and Test Cases
+
+console.log(sumOfNestedArray([1, [2, 3], [4, [5, 6]]])); // 21
+console.log(sumOfNestedArray([10, [20, 30, [40]], 50])); // 150
 
 Algorithm
-create function sumOfNestedArray which takes nestedArray argument
-create variable stack equal to copy of array
-create variable totalSum equal to zero
-while stack has length
-  create variable register equal to last element of stack, removed from stack
-  if register is array
-    add a spread of register to stack
-  else
-    add element to totalSum
-return totalSum
+  - Create function sumOfNestedArray which defines inputArray as parameter
+    - define stack variable equal to copy of array
+    - define total sum equal to 0
+    - while stack contains elements
+      - let current element equal last element of stack
+      - if current element is array
+        - add to stack - copy of current element
+        - else
+          - add current element to total sum
+    - return total sum
 */
-
-function sumOfNestedArray(nestedArray) {
-  let stack = [...nestedArray];
+function sumOfNestedArray(inputArray) {
+  let stack = [...inputArray];
   let totalSum = 0;
   while (stack.length) {
-    let register = stack.pop();
-    if (Array.isArray(register)) {
-      stack.push(...register);
+    let currentElement = stack.pop();
+    if (Array.isArray(currentElement)) {
+      stack.push(...currentElement);
     } else {
-      totalSum += register;
+      totalSum += currentElement;
     }
   }
   return totalSum;
 }
-// Test cases
 console.log(sumOfNestedArray([1, [2, 3], [4, [5, 6]]])); // 21
 console.log(sumOfNestedArray([10, [20, 30, [40]], 50])); // 150
